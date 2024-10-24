989006fc817e82876a9fbe3c8f4c91fe03ca0c73
MK <newlink05@mail.ru>
2024-10-24 18:19:33 +0500

    reformat code

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,8 +1,8 @@
 public class CreditPaymentService {
 
     public int calculate(int creditAmountRub, int creditDurationMonths) {
-        double percentMonth = 9.99/ 100 / 12;
-        double monthlyPayment = creditAmountRub * percentMonth / (1 - Math.pow(1 + percentMonth, - creditDurationMonths));
+        double percentMonth = 9.99 / 100 / 12;
+        double monthlyPayment = creditAmountRub * percentMonth / (1 - Math.pow(1 + percentMonth, -creditDurationMonths));
         int result = (int) monthlyPayment;
         return result;
     }
